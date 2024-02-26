b00d54630aeb5f40fc400d0e5a44b57e86b28c3a
mrochny <tpalinski@icloud.com>
2024-02-26 14:37:00 +0100

    BSK-10 Key generation

---
+++ ttp-server/src/crypto_server/mod.rs
@@ -0,0 +1,42 @@
+use aes_gcm::{aead::Aead, AeadCore, Aes256Gcm, Key, KeyInit};
+use base64ct::{Base64, Encoding};
+use rand::rngs::OsRng;
+use rsa::{pkcs1::EncodeRsaPublicKey, pkcs8::EncodePrivateKey, RsaPrivateKey, RsaPublicKey};
+use sha2::{Digest, Sha256};
+
+use self::model::UserKeys;
+
+pub mod model;
+
+pub fn generate_keys(pin: &str) -> UserKeys{
+    let (public_key, private_key) = generate_rsa_keys();
+    let hashed_private_key = encrypt_private_key(pin, &private_key);
+    let public_key = public_key.to_pkcs1_pem(rsa::pkcs8::LineEnding::LF).unwrap();
+    UserKeys::new(public_key, hashed_private_key)
+}
+
+fn generate_rsa_keys() -> (RsaPublicKey, RsaPrivateKey) {
+    let mut rng = rand::thread_rng();
+    let bits = 4096;
+
+    let private_key = RsaPrivateKey::new(&mut rng, bits).expect("Error while creating private key");
+    let public_key = RsaPublicKey::from(&private_key);
+    (public_key, private_key)
+}
+
+fn encrypt_private_key(pin: &str, key: &RsaPrivateKey) -> String {
+    println!("Encrypting private key...");
+    let pin_hash = generate_aes_key(pin);
+    let cipher = Aes256Gcm::new(&pin_hash);
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+    let key_as_bytes = key.to_pkcs8_pem(rsa::pkcs8::LineEnding::LF).unwrap();
+    let key_as_bytes = key_as_bytes.as_bytes();
+    let encrypted_key = cipher.encrypt(&nonce, key_as_bytes).expect("Error while encrypting private key");
+    println!("Private key encrypted successfully");
+    Base64::encode_string(&encrypted_key)
+}
+
+fn generate_aes_key(pin: &str) -> Key<Aes256Gcm> {
+    let hashed = Sha256::digest(pin);
+    hashed.into()
+}
---
+++ ttp-server/src/pages/generate_keys.rs
@@ -0,0 +1,61 @@
+use leptos::*;
+
+use crate::crypto_server::{generate_keys, model::UserKeys};
+
+#[server(GenerateUserKeys, "/api")]
+pub async fn generate_user_keys(pin: String) -> Result<UserKeys, ServerFnError> {
+    println!("geneating keys");
+    let keys = generate_keys(&pin);
+    println!("finished generating keys");
+    Ok(keys)
+}
+
+#[component]
+fn KeyDisplay(keys: Signal<UserKeys>) -> impl IntoView {
+    view! {
+        <p> Public key: {move || keys.get().keys().0} </p>
+        <p> Private key: {move || keys.get().keys().1} </p>
+    }
+}
+
+pub fn GenerateKeys() -> impl IntoView {
+
+    let (user_pin, set_user_pin) = create_signal("".to_owned());
+    let generate_action = create_server_action::<GenerateUserKeys>();
+    let on_click = move |_| {
+        generate_action.dispatch(GenerateUserKeys{pin: user_pin.get()});
+    };
+    let keys = Signal::derive(move ||{
+        let res = generate_action.value().get();
+        match res {
+            Some(value) => value.unwrap(),
+            None => UserKeys::new(String::new(), String::new())
+        }
+    });
+
+    view! {
+        <div class="flex flex-center items-center justify-center flex-col gap-1 text-center">
+            <h1 class="text-violet-300 text-3xl"> Generate your encryption keys </h1>
+            <Show
+                when=move || {!generate_action.pending().get()}
+                fallback=|| view! {"Generating keys. This process may take a while"}
+            >
+                <h2 class="bold text-xl text-center"> Please enter your unique password. It will be used during encryption process, so please store it somewhere securely, as you will be required to provide it while signing documents</h2>
+                <input class="border border-violet-700" type="text"
+                    on:input=move |ev| {
+                        set_user_pin(event_target_value(&ev));
+                    }
+                prop:value=user_pin
+                />
+                <button class="pd-12 bg-violet-300 rounded-xl" on:click=on_click> Generate keys </button>
+            </Show>
+            <Show
+                when=move || {(!generate_action.pending().get()) && generate_action.version().get() > 0}
+                fallback=|| view! {}
+            >
+                <KeyDisplay keys=keys/>
+            </Show>
+        </div>
+    }
+}
+
