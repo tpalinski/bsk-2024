282bf113db8d4826ba60df835784e1283716f1b1
tpalinski <tpalinski@icloud.com>
2024-02-27 21:59:33 +0100

    BSK-11 User data persistence

--- ttp-server/src/lib.rs
+++ ttp-server/src/lib.rs
@@ -1,6 +1,12 @@
 pub mod app;
 pub mod pages;
+pub mod model;
+#[cfg(feature = "ssr")]
 pub mod crypto_server;
+#[cfg(feature = "ssr")]
+pub mod user_repository;
+#[cfg(feature = "ssr")]
+pub mod key_api;
 
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
