499b6b2e1b240d380743d6c1474a46e4e4d810f8
tpalinski <tpalinski@icloud.com>
2024-03-09 14:12:11 +0100

    BSK-7 Landing page styling

--- ttp-server/src/pages/home_page.rs
+++ ttp-server/src/pages/home_page.rs
@@ -6,7 +6,8 @@ pub fn HomePage() -> impl IntoView {
     view! {
         <div class="bg-gray-600 h-screen w-screen flex flex-col gap-4 items-center">
             <h1 class="text-violet-300 text-6xl"> "BSK2024 - TTP server (real)" </h1>
-            <a href="/generate" class="pd-6 rounded-xl bg-grey-800"> "Generate keys" </a>
+            <p class="bold text-xl text-center"> "This site plays the role of a Trusted Third Party (TTP). It provides the users with encryption keys, stores them and automatically provides them to users during encryption process." </p>
+            <a href="/generate" class="p-4 bg-violet-300 rounded-xl"> "Get your encryption keys" </a>
         </div>
     }
 }
