2e9a5b22a17c28e96a6418b9896b64d47ff525cc
tpalinski <tpalinski@icloud.com>
2024-04-04 12:15:07 +0200

    [BSK-37] Add login form

--- client/src/pages/login.rs
+++ client/src/pages/login.rs
@@ -11,7 +11,7 @@ async fn server_login(email: String, password: String) -> Result<LoginResponse, 
         Err(status) => {
             let resp = expect_context::<leptos_actix::ResponseOptions>();
             resp.set_status(actix_web::http::StatusCode::from_u16(status.as_u16()).unwrap());
-            return Err(ServerFnError::ServerError("Error while logging in".to_owned()))
+            return Err(ServerFnError::ServerError("Wrong credentials were provided".to_owned()))
         }
     }
 }
@@ -19,10 +19,12 @@ async fn server_login(email: String, password: String) -> Result<LoginResponse, 
 #[component]
 pub fn LoginPage() -> impl IntoView {
 
-    let login_action = create_server_action::<ServerLogin>();
+    let (email, set_email) = create_signal(String::new());
+    let (password, set_password) = create_signal(String::new());
 
+    let login_action = create_server_action::<ServerLogin>();
     let handle_click = move |_| {
-        login_action.dispatch(ServerLogin{email: "testowy2@mail.com".to_owned(), password: "haslomaslo".to_owned()});
+        login_action.dispatch(ServerLogin{email: email.get(), password: password.get()});
     };
 
     create_effect(move |_| {
@@ -33,7 +35,7 @@ pub fn LoginPage() -> impl IntoView {
                     let state = expect_context::<RwSignal<GlobalState>>();
                     state.set(GlobalState{email: res.email, token: res.token, name: res.name});
                     let navigate = leptos_router::use_navigate();
-                    navigate("/", Default::default());
+                    navigate("/signature", Default::default());
                 },
                 Err(e) => {
                     console_log(&e.to_string())
@@ -42,11 +44,53 @@ pub fn LoginPage() -> impl IntoView {
         }
     });
 
+    let login_error = Signal::derive(move || {
+        let val = login_action.value().get();
+        if val.is_some() {
+            match val.unwrap() {
+                Ok(_) => None,
+                Err(e) => Some(e.to_string())
+            }
+        } else {
+            None
+        }
+    });
+
     view! {
         <div class="bg-gray-600 h-screen w-screen flex flex-col gap-4 items-center">
             <h1 class="text-violet-300 text-6xl">"Professional RSA encryption app - login page"</h1>
-            <p class="bold text-xl text-center"> Press the button to log in</p>
-            <button on:click=handle_click class="p-4 bg-violet-300 rounded-xl"> Sign a document utilizing electronic signature </button>
+            <p class="bold text-xl text-center"> Please log in to access desired functionality</p>
+            <label>
+                "Email"
+                <input type="email" name="email"
+                    on:input=move |ev| {
+                        set_email(event_target_value(&ev));
+                    }
+                    prop:value=email
+                />
+            </label>
+            <label>
+                "Password"
+                <input type="password" name="password"
+                    on:input=move |ev| {
+                        set_password(event_target_value(&ev));
+                    }
+                    prop:value=password
+
+                />
+            </label>
+            <Show 
+                when=move || {login_error.get().is_some()}
+                fallback=|| view! {}
+            >
+                {login_error.get().unwrap()}
+            </Show>
+            <Show
+                when=move || {!login_action.pending().get()}
+                fallback=|| view! {<div class="p-4 bg-violet-300 rounded-xl"> Loading... </div>}
+            >
+                <button on:click=handle_click class="p-4 bg-violet-300 rounded-xl"> Log in </button>
+            </Show> 
         </div>
     }
 }
--- client/src/pages/signature.rs
+++ client/src/pages/signature.rs
@@ -1,5 +1,7 @@
 use leptos::*;
 
+use crate::app::GlobalState;
+
 #[server(SignFile, "/api")]
 async fn sign_file(path: String) -> Result<(), ServerFnError> {
     #[cfg(feature="ssr")]
@@ -27,8 +29,17 @@ pub fn SignaturePage() -> impl IntoView {
     let input_ref: NodeRef<html::Input> = create_node_ref();
     let sign_action = create_server_action::<SignFile>();
 
+    let global_state = expect_context::<RwSignal<GlobalState>>();
+
+    if global_state.get().token.is_empty() {
+        let navigate = leptos_router::use_navigate();
+        navigate("/login", Default::default());
+    }
+
     view! {
         <div class="bg-gray-600 h-screen w-screen flex flex-col gap-4 items-center">
+            <h1 class="text-violet-300 text-6xl">"Professional RSA encryption app"</h1>
+            <p class="bold text-xl text-center"> Welcome {global_state.get().name}. Please choose the file you want to sign</p>
             <input type="file" 
                 node_ref=input_ref
             />
