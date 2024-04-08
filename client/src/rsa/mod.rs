98c48bc3183b19b40bd6961fe0b361e34b29c56a
tpalinski <tpalinski@icloud.com>
2024-04-08 10:07:38 +0200

    [BSK-47] Automatic private key fetching

--- client/src/rsa/mod.rs
+++ client/src/rsa/mod.rs
@@ -8,15 +8,15 @@ mod cert;
 mod utils;
 pub mod model;
 
-pub async fn sign_data(data: &[u8], author: String, token: String) -> Result<String, String> {
-    let (key, new_token) = get_private_key(author.clone(), token).await?;
+pub async fn sign_data(data: &[u8], author: String, token: String, pin: String) -> Result<(String, String), String> {
+    let (key, new_token) = get_private_key(author.clone(), token, pin).await?;
     let props = SignatureProps{
         author,
         data,
         pkey: key
     };
     let encrypted = Signature::generate(props);
-    Ok(encrypted.to_xml())
+    Ok((encrypted.to_xml(), new_token))
 }
 
 pub fn verify_signature(data: Vec<u8>, signature: String) -> Result<(), ()> {
