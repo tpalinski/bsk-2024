dc7f9fc54d945b7863d5fd0fdb113ef9422f75e2
tpalinski <tpalinski@icloud.com>
2024-04-08 10:00:08 +0200

    [BSK-46] XML generation

--- client/src/rsa/model.rs
+++ client/src/rsa/model.rs
@@ -1,16 +1,21 @@
+use rsa::RsaPrivateKey;
 use serde::{Deserialize, Serialize};
+
+use super::{encryption, utils::get_hash};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 #[serde(rename="ds:Signature")]
 pub struct Signature {
     #[serde(rename="ds:SignedInfo")]
     info: SignedInfo,
+    #[serde(rename="ds:SignatureValue")]
+    sig: String,
     #[serde(rename="ds:Object")]
     obj: Object
 }
 
-impl Signature {
-    pub fn new() -> Self {
+impl Default for Signature {
+    fn default() -> Self {
         Signature {
             info: SignedInfo {
                 cm: AlgoTag{ 
@@ -26,18 +31,55 @@ impl Signature {
                     dv: "hash".to_owned()
                 }
             },
+            sig: "signature".to_owned(),
             obj: Object {
                 qp: QualifyingProps {
                     sp: SignedProperties {
                         ssp: SignedSignatureProperties {
                             cert: "certificate".to_owned(),
-                            city: "city".to_owned(),
-                            time: "now".to_owned()
+                            city: "Gdansk".to_owned(),
+                            time: format!("{:?}", chrono::offset::Local::now())
                         }
                     }
                 }
             }
         }
+    }
+}
+
+pub struct SignatureProps<'a> {
+    pub pkey: RsaPrivateKey,
+    pub data: &'a[u8],
+    pub author: String
+}
+
+impl Signature {
+    fn set_author(mut self, author: String) -> Self {
+        self.obj.qp.sp.ssp.cert = author;
+        self
+    }
+
+    fn set_hash(mut self, data: &[u8]) -> Self {
+        self.info.rf.dv = get_hash(data);
+        self
+    }
+
+    fn set_signature(mut self, data: &[u8], key: RsaPrivateKey) -> Self {
+        let sig = encryption::sign(data, key);
+        self.sig = sig;
+        self
+    }
+
+    pub fn generate(props: SignatureProps) -> Self {
+        let sig = Signature::default();
+        sig
+            .set_author(props.author)
+            .set_hash(&props.data)
+            .set_signature(&props.data, props.pkey)
+    }
+
+    pub fn to_xml(&self) -> String {
+        quick_xml::se::to_string(self).unwrap()
     }
 }
 
